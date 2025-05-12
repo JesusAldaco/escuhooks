2f9bac164d106efa6b3e9827fd3c2efaae4eb9cd
JesusAldaco <jesus.ibanez@g-tech.io>
2025-05-12 08:37:21 -0600

    creaciòn de maestros

---
+++ convex/schema.ts
@@ -0,0 +1,15 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  estudiantes: defineTable({
+    numMatricula: v.string(),
+    nombre: v.string(),
+    correo: v.string(),
+  }),
+  maestros: defineTable({
+    numEmpleado: v.string(),
+    nombre: v.string(),
+    correo: v.string(),
+  })
+});
